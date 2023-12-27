e7d4fa363f3acdc150af4c6ff02de7be4e81cf8d
yeeun <yeeun.park1@outlook.com>
2023-12-27 15:56:48 +0900

    hospital

---
+++ springboard3/src/test/java/com/aidata/springboard3/Springboard3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidata.springboard3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboard3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
